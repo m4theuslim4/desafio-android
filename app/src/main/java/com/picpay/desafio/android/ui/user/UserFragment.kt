9eb11d94d7411431ad1c6c8f8e8b0491b56a30f6
Matheus Lima <matheuslima9412@gmail.com>
2022-07-01 18:35:28 -0300

    adds support for jetpack navigation

---
+++ app/src/main/java/com/picpay/desafio/android/ui/user/UserFragment.kt
@@ -0,0 +1,90 @@
+package com.picpay.desafio.android.ui.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.data.model.User
+import com.picpay.desafio.android.data.network.PicPayService
+import com.picpay.desafio.android.ui.UserListAdapter
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class UserFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var adapter: UserListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+
+    private val gson: Gson by lazy { GsonBuilder().create() }
+
+    private val okHttp: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    private val service: PicPayService by lazy {
+        retrofit.create(PicPayService::class.java)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        recyclerView = requireView().findViewById(R.id.recyclerView)
+        progressBar = requireView().findViewById(R.id.user_list_progress_bar)
+
+        adapter = UserListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        progressBar.visibility = View.VISIBLE
+        service.getUsers()
+            .enqueue(object : Callback<List<User>> {
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                    val message = getString(R.string.error)
+
+                    progressBar.visibility = View.GONE
+                    recyclerView.visibility = View.GONE
+
+                    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    progressBar.visibility = View.GONE
+
+                    adapter.users = response.body()!!
+                }
+            })
+    }
+}
