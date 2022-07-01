1570296712449f11bc13a3973e51e65c3a58913f
Matheus Lima <matheuslima9412@gmail.com>
2022-07-01 19:07:24 -0300

    adds viewmodel for user flow with hilt injector annotanion

---
+++ app/src/main/java/com/picpay/desafio/android/ui/user/UserViewModel.kt
@@ -0,0 +1,8 @@
+package com.picpay.desafio.android.ui.user
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class UserViewModel : ViewModel() {
+}
